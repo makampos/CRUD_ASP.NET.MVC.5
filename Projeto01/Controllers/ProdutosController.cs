30ccb61cda60199a0fb2006107c609960b6f961d
Matheus de Campos <matheus.campos@edu.unifil.br>
2019-11-30 21:44:02 -0300

    Implementação de novas telas para seção produtos e partial view

--- Projeto01/Controllers/ProdutosController.cs
+++ Projeto01/Controllers/ProdutosController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
 using Projeto01.Models;
+using System.Net;
 
 namespace Projeto01.Controllers
 {
@@ -20,9 +21,20 @@ namespace Projeto01.Controllers
         }
 
         // GET: Produtos/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(long? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult (HttpStatusCode.BadRequest);
+            }
+
+            Produto produto = context.Produtos.Where(p => p.ProdutoId == id).Include(c => c.Categoria).Include(f => f.Fabricante).First();
+
+            if ( id == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
         }
 
         // GET: Produtos/Create
@@ -52,45 +64,71 @@ namespace Projeto01.Controllers
         }
 
         // GET: Produtos/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(long? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Produto produto = context.Produtos.Find(id);
+
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.CategoriaId = new SelectList(context.Categorias.OrderBy(b => b.Nome), "CategoriaId", "Nome", produto.CategoriaId);
+            ViewBag.FabricanteId = new SelectList(context.Fabricantes.OrderBy(b => b.Nome), "FabricanteId", "Nome", produto.FabricanteId);
+            return View(produto);
         }
 
         // POST: Produtos/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(Produto produto)
         {
             try
             {
                 // TODO: Add update logic here
-
+                if (ModelState.IsValid)
+                    context.Entry(produto).State = EntityState.Modified;
+                context.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
             {
-                return View();
+                return View(produto);
             }
         }
 
         // GET: Produtos/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(long? id)
         {
-            return View();
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = context.Produtos.Where(p => p.ProdutoId == id).Include(c => c.Categoria).Include(f => f.Fabricante).First();
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
         }
 
         // POST: Produtos/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(long id )
         {
-            try
-            {
-                // TODO: Add delete logic here
-
+            try {
+                Produto produto = context.Produtos.Find(id);
+                context.Produtos.Remove(produto);
+                context.SaveChanges();
+                TempData["Message"] = "Produto" + produto.Nome.ToUpper() + " foi removido";
                 return RedirectToAction("Index");
-            }
-            catch
-            {
+            } 
+            catch {
                 return View();
             }
         }
